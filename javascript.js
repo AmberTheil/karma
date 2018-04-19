9465bb2b0605f28d9b1947d46c2ba2e7156afdb2
Amber Theil <ambertheil@tadilie-x1c.europe.corp.microsoft.com>
2018-04-19 08:45:30 +0200

    hey

--- javascript.js
+++ javascript.js
@@ -26,12 +26,11 @@ function gorillaSit() {
 
     $("#abe_container").addClass("moving_abe");
     $("#abe_sprite").addClass("walk_banana_abe");
+    $("#gorilla_sprite").addClass("gorilla_spis");
 
     $("#abe_container").on("animationend", abeVred);
 
 }
-
-
 
 //Abe bliver vred
 function abeVred() {
@@ -39,11 +38,10 @@ function abeVred() {
 
     $("#abe_container").off("animationend", abeVred);
 
+    $("#gorilla_sprite").removeClass("sit_gorilla");
     $("#abe_sprite").removeClass("walk_banana_abe");
 
     $("#abe_sprite").addClass("abe_vred");
-    $("#gorilla_sprite").addClass("gorilla_spis");
-
     $("#gorilla_container").on("animationend", gorillaAv);
 }
 
@@ -53,14 +51,71 @@ function gorillaAv() {
     $("#gorilla_container").off("animationend", gorillaAv);
 
     $("#gorilla_sprite").removeClass("gorilla_spis");
+    $("#gorilla_container").removeClass("moving_gorilla");
 
-    $("#skildpadde").show();
     $("#gorilla_container").addClass("moving_gorilla2");
     $("#gorilla_sprite").addClass("walk_gorilla");
+
     $("#skildpadde").addClass("skildpadde_falder");
+    $("#skildpadde").show();
+
+    $("#gorilla_container").on("animationend", gorillafaldt);
+}
+
+function gorillafaldt() {
+    console.log("gorilla er faldet");
+
+    $("#gorilla_container").off("animationend", gorillafaldt);
+    $("#gorilla_sprite").removeClass("walk_gorilla");
+
+    $("#gorilla_sprite").addClass("gorilla_faldt");
+    $("#gorilla_sprite").addClass("gorilla_faldt");
+    $("#gorilla_sprite").addClass("gorilla_cry");
+
+    $("#gorilla_sprite").on("animationend", abeGriner);
+}
+
+function abeGriner() {
+    console.log("abeGriner");
+
+    $("#gorilla_sprite").off("animationend", abeGriner);
+    $("#abe_sprite").removeClass("abe_vred");
+
+    $("#abe_sprite").addClass("abe_griner");
+
+    $("#abe_sprite").on("animationend", abe_plaster);
+
+}
+
+function abe_plaster() {
+    console.log("abe plaster");
+
+    $("#abe_sprite").off("animationend", abe_plaster);
+    $("#abe_sprite").removeClass("abe_griner");
+    $("#gorilla_sprite").removeClass("gorilla_cry");
+    $("#gorilla_sprite").removeClass("gorilla_faldt");
+
+    $("#gorilla_sprite").addClass("gorilla_plaster");
+    $("#abe_container").addClass("moving_abe2");
+    $("#abe_sprite").addClass("walk_abe");
+    $("#abe_sprite").addClass("abe_plaster");
+
+    $("#abe_sprite").on("baggrund_ryk");
+}
+
+function baggrund_ryk() {
+    console.log("baggrund_ryk");
+
+    $("#abe_sprite").off("baggrund_ryk");
+    $("#abe_sprite").removeClass("abe_plaster");
+
+    $("#baggrund").addclass("baggrund_moving");
+
+
+}
 
 
 
-
+function styletile() {
 
 }
